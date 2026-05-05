3b9956c10b3c642b4e36a13f22a56125eb72e2f1
Robert Soliday <soliday@anl.gov>
2026-05-05 13:04:59 -0500

    Element-specific output files had a bug that caused %s sequences to the evaluated and then saved in evaluated form to parameter and lattice files.
    Original author: borland

--- src/rfmode.cc
+++ src/rfmode.cc
@@ -1162,12 +1162,13 @@ void set_up_rfmode(RFMODE *rfmode, char *element_name, double element_z, long n_
     if (rfmode->record && strlen(rfmode->record)==0)
       rfmode->record = NULL;
     if (rfmode->record) {
-      rfmode->record = compose_filename(rfmode->record, run->rootname);
+      char *filename;
+      filename = compose_filename(rfmode->record, run->rootname);
 #if (USE_MPI)
       if (myid == 0)
 #endif
         if (!(rfmode->SDDSrec = (SDDS_DATASET *)tmalloc(sizeof(*(rfmode->SDDSrec)))) ||
-            !SDDS_InitializeOutputElegant(rfmode->SDDSrec, SDDS_BINARY, 1, NULL, NULL, rfmode->record) ||
+            !SDDS_InitializeOutputElegant(rfmode->SDDSrec, SDDS_BINARY, 1, NULL, NULL, filename) ||
             SDDS_DefineParameter(rfmode->SDDSrec, (char *)"SVNVersion", NULL, NULL, (char *)"SVN version number", NULL, SDDS_STRING, (char *)SVN_VERSION) < 0 ||
             SDDS_DefineColumn(rfmode->SDDSrec, (char *)"Bunch", NULL, NULL, (char *)"Bunch number", NULL, SDDS_LONG, 0) < 0 ||
             SDDS_DefineColumn(rfmode->SDDSrec, (char *)"Pass", NULL, NULL, NULL, NULL, SDDS_LONG, 0) < 0 ||
@@ -1189,16 +1190,18 @@ void set_up_rfmode(RFMODE *rfmode, char *element_name, double element_z, long n_
           SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
           SDDS_Bomb((char *)"problem setting up RFMODE record file");
         }
+      free(filename);
     }
     if (rfmode->feedbackRecordFile && strlen(rfmode->feedbackRecordFile)==0)
       rfmode->feedbackRecordFile = NULL;
     if (rfmode->driveFrequency > 0 && rfmode->feedbackRecordFile) {
-      rfmode->feedbackRecordFile = compose_filename(rfmode->feedbackRecordFile, run->rootname);
+      char *filename;
+      filename = compose_filename(rfmode->feedbackRecordFile, run->rootname);
 #if (USE_MPI)
       if (myid == 1)
 #endif
         if (!(rfmode->SDDSfbrec = (SDDS_DATASET *)tmalloc(sizeof(*(rfmode->SDDSfbrec)))) ||
-            !SDDS_InitializeOutputElegant(rfmode->SDDSfbrec, SDDS_BINARY, 1, NULL, NULL, rfmode->feedbackRecordFile) ||
+            !SDDS_InitializeOutputElegant(rfmode->SDDSfbrec, SDDS_BINARY, 1, NULL, NULL, filename) ||
             SDDS_DefineParameter(rfmode->SDDSfbrec, (char *)"SVNVersion", NULL, NULL, (char *)"SVN version number", NULL, SDDS_STRING, (char *)SVN_VERSION) < 0 ||
             SDDS_DefineColumn(rfmode->SDDSfbrec, (char *)"Pass", NULL, NULL, NULL, NULL, SDDS_LONG, 0) < 0 ||
             SDDS_DefineColumn(rfmode->SDDSfbrec, (char *)"t", NULL, (char *)"s", (char *)"Time", NULL, SDDS_DOUBLE, 0) < 0 ||
@@ -1216,6 +1219,7 @@ void set_up_rfmode(RFMODE *rfmode, char *element_name, double element_z, long n_
           SDDS_PrintErrors(stderr, SDDS_VERBOSE_PrintErrors);
           SDDS_Bomb((char *)"problem setting up RFMODE feedback record file");
         }
+      free(filename);
     }
     rfmode->fileInitialized = 1;
   }
