e52b43f61eef974627ad7dc4a1aa34981d321f51
rtsoliday <soliday@anl.gov>
2026-07-01 09:05:35 -0500

    Refactored with Claude Code to eliminate duplicate procedures. Added procedure to compute effective emittance, beta, and alpha for use with Lindberg's method.
    Original author: borland

--- physics/brightness.c
+++ physics/brightness.c
@@ -139,149 +139,109 @@ long ReadTwissInput(char *inputfile, TWISS_PARAMETER *twiss, double coupling, do
 }
 
 
-long GetTwissValues(SDDS_DATASET *SDDSin, 
-                    double *betax, double *alphax, double *etax, double *etaxp, 
-                    double *betay, double *alphay, double *etay, double *etayp, 
-                    double *ex0, double *ey0, double *Sdelta0, double *pCentral, 
-		    double emitRatio, double coupling)
-{
+long GetTwissValues(SDDS_DATASET *SDDSin,
+                    double *betax, double *alphax, double *etax, double *etaxp,
+                    double *betay, double *alphay, double *etay, double *etayp,
+                    double *ex0, double *ey0, double *Sdelta0, double *pCentral,
+                    double emitRatio, double coupling) {
   double *data;
-  int32_t rows, ey0Exist=0;
+  long rows, ey0Exist = 0;
 
-  if (!betax || !alphax || !etax || !etaxp || !betay || !alphay || !etay || !etayp ||
-      !ex0 || !ey0 || !Sdelta0 || !pCentral) {
-    fprintf(stderr, "Null pointer passed to GetTwissValues\n");
+  if (!(rows = SDDS_RowCount(SDDSin)))
     return 0;
-  }
 
-  if (!(rows=SDDS_RowCount(SDDSin)))
-    return 0;
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "betax"))) {
-    fprintf(stderr,"unable to get betax\n");
-    return 0;
-  }
-  *betax = data[rows-1];
-  free(data);
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "alphax"))) {
-    fprintf(stderr, "unable to get alphax.\n");
-    return 0;
-  }
-  *alphax = data[rows-1];
-  free(data);
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "etax"))) {
-    fprintf(stderr, "unable to get etax.\n");
-    return 0;
-  }
-  *etax = data[rows-1];
-  free(data);
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "etaxp"))) {
-    fprintf(stderr,"unable to get etax.\n");
-    return 0;
-  }
-  
-  *etaxp = data[rows-1];
-  free(data);
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "betay"))) {
-    fprintf(stderr,"unable to get betay.\n");
-    return 0;
-  }
-  
-  *betay = data[rows-1];
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "betax")))
+    SDDS_Bomb("unable to get betax");
+  *betax = data[rows - 1];
   free(data);
 
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "alphay"))) {
-    fprintf(stderr,"unable to get alphay.\n");
-    return 0;
-  }
-  
-  *alphay = data[rows-1];
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "alphax")))
+    SDDS_Bomb("unable to get alphax");
+  *alphax = data[rows - 1];
   free(data);
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "etay"))) {
-    fprintf(stderr,"unable to get etay.\n");
-    return 0;
-  }
-  
-  *etay = data[rows-1];
+
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "etax")))
+    SDDS_Bomb("unable to get etax");
+  *etax = data[rows - 1];
   free(data);
-  
-  if (!(data=SDDS_GetColumnInDoubles(SDDSin, "etayp"))) {
-    fprintf(stderr,"unable to get etay.\n");
-    return 0;
-  }
-  
-  *etayp = data[rows-1];
+
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "etaxp")))
+    SDDS_Bomb("unable to get etax");
+  *etaxp = data[rows - 1];
   free(data);
-  if (SDDS_CheckParameter(SDDSin, "ex0", "$gp$rm", SDDS_ANY_FLOATING_TYPE, NULL)==SDDS_CHECK_OK) {
-    if (!SDDS_GetParameterAsDouble(SDDSin, "ex0", ex0)) {
-      fprintf(stderr,"unable to get ex0 parameter from input file.\n");
-      return 0;
-    }
+
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "betay")))
+    SDDS_Bomb("unable to get betay");
+  *betay = data[rows - 1];
+  free(data);
+
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "alphay")))
+    SDDS_Bomb("unable to get alphay");
+  *alphay = data[rows - 1];
+  free(data);
+
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "etay")))
+    SDDS_Bomb("unable to get etay");
+  *etay = data[rows - 1];
+  free(data);
+
+  if (!(data = SDDS_GetColumnInDoubles(SDDSin, "etayp")))
+    SDDS_Bomb("unable to get etay");
+  *etayp = data[rows - 1];
+  free(data);
+  if (SDDS_CheckParameter(SDDSin, "ex0", "$gp$rm", SDDS_ANY_FLOATING_TYPE, NULL) == SDDS_CHECK_OK ||
+      SDDS_CheckParameter(SDDSin, "ex0", "m", SDDS_ANY_FLOATING_TYPE, NULL) == SDDS_CHECK_OK) {
+    if (!SDDS_GetParameterAsDouble(SDDSin, "ex0", ex0))
+      SDDS_Bomb("unable to get ex0 parameter from input file");
   } else {
-    if (!(data=SDDS_GetColumnInDoubles(SDDSin, "ex"))) {
-      fprintf(stderr,"unable to get ex.\n");
-      return 0;
-    }
+    if (!(data = SDDS_GetColumnInDoubles(SDDSin, "ex")))
+      SDDS_Bomb("unable to get ex");
     *ex0 = data[0];
     free(data);
   }
-  if (SDDS_CheckParameter(SDDSin, "pCentral", NULL, SDDS_ANY_FLOATING_TYPE, NULL)==SDDS_CHECK_OK) {
-    if (!SDDS_GetParameterAsDouble(SDDSin, "pCentral", pCentral)) {
-      fprintf(stderr,"unable to get pCentral parameter from input file.\n");
-      return 0;
-    }
+  if (SDDS_CheckParameter(SDDSin, "pCentral", NULL, SDDS_ANY_FLOATING_TYPE, NULL) == SDDS_CHECK_OK) {
+    if (!SDDS_GetParameterAsDouble(SDDSin, "pCentral", pCentral))
+      SDDS_Bomb("unable to get pCentral parameter from input file");
   } else {
-    if (!(data=SDDS_GetColumnInDoubles(SDDSin, "pCentral"))) {
-      fprintf(stderr,"unable to get pCentral.\n");
-      return 0;
-    }
+    if (!(data = SDDS_GetColumnInDoubles(SDDSin, "pCentral")))
+      SDDS_Bomb("unable to get pCentral");
     *pCentral = data[0];
     free(data);
   }
-  if (SDDS_CheckParameter(SDDSin, "Sdelta0", "", SDDS_ANY_FLOATING_TYPE, NULL)==SDDS_CHECK_OK) {
-    if (!SDDS_GetParameterAsDouble(SDDSin, "Sdelta0", Sdelta0)) {
-      fprintf(stderr,"unable to get Sdelta0 parameter from input file.\n");
-      return 0;
-    }
+  if (SDDS_CheckParameter(SDDSin, "Sdelta0", "", SDDS_ANY_FLOATING_TYPE, NULL) == SDDS_CHECK_OK) {
+    if (!SDDS_GetParameterAsDouble(SDDSin, "Sdelta0", Sdelta0))
+      SDDS_Bomb("unable to get Sdelta0 parameter from input file");
   } else {
-    if (!(data=SDDS_GetColumnInDoubles(SDDSin, "Sdelta"))) {
-      fprintf(stderr,"unable to get pCentral.\n");
-      return 0;
-    }
+    if (!(data = SDDS_GetColumnInDoubles(SDDSin, "Sdelta")))
+      SDDS_Bomb("unable to get pCentral");
     *Sdelta0 = data[0];
     free(data);
   }
   /* get ey0 if it is there */
-  if (SDDS_CheckParameter(SDDSin, "ey0", "", SDDS_ANY_FLOATING_TYPE, NULL)==SDDS_CHECK_OK) {
+  if (SDDS_GetParameterAsDouble(SDDSin, "ey0", ey0)) {
     ey0Exist = 1;
-    if (!SDDS_GetParameterAsDouble(SDDSin, "ey0", ey0)) {
-      fprintf(stderr, "unable to get ey0\n");
-      return 0;
-    }
-  } else if ((data=SDDS_GetColumnInDoubles(SDDSin, "ey"))) {
-    ey0Exist=1;
+  } else if ((data = SDDS_GetColumnInDoubles(SDDSin, "ey"))) {
+    ey0Exist = 1;
     *ey0 = data[0];
     free(data);
   }
-  if (*ex0<=0.0) {
-    fprintf(stderr,"ex0 should be greater than zero.\n");
-    return 0;
-  }
+  if (*ex0 <= 0.0)
+    SDDS_Bomb("ex0 should be greater than zero.");
   if (!ey0Exist) {
-    if (emitRatio==0 && coupling==0) {
-      fprintf(stderr, "No vertical emittance data in file: give -emittanceRatio or -coupling.\n");
-      return 0;
+    double Jx, Jy;
+    if (SDDS_CheckParameter(SDDSin, "Jx", NULL, SDDS_ANY_FLOATING_TYPE, NULL) != SDDS_CHECK_OK ||
+        SDDS_CheckParameter(SDDSin, "Jy", NULL, SDDS_ANY_FLOATING_TYPE, NULL) != SDDS_CHECK_OK ||
+        !SDDS_GetParameterAsDouble(SDDSin, "Jx", &Jx) ||
+        !SDDS_GetParameterAsDouble(SDDSin, "Jy", &Jy)) {
+      SDDS_Bomb("unable to get Jx and/or Jy parameter from input file");
     }
+    if (emitRatio == 0 && coupling == 0)
+      SDDS_Bomb("No vertical emittance data in file: give -emittanceRatio or -coupling");
     if (coupling) {
-      *ex0 = *ex0/(1+coupling);
-      *ey0 = coupling*(*ex0);
+      *ex0 = *ex0 / (1 + Jy * coupling / Jx);
+      *ey0 = coupling * (*ex0);
     } else {
-      *ey0 = *ex0*emitRatio;
+      *ey0 = *ex0 * emitRatio;
     }
   }
   return 1;
@@ -316,75 +276,108 @@ void ComputeBeamSize(double period, long Nu, double ex, double ey, double Sdelta
     *Sy = sqrt(ey*betay + sqr(Sdelta0*etay))*1.0e3;
 }
 
-void FindPeak(double *E,double *spec,double *ep,double *sp,long n)
+/* Find the peak of (E,spec) over n points.  If fwhm is non-NULL, also
+ * write the FWHM (in E units) to *fwhm, or -1 if it cannot be resolved. */
+void FindPeak(double *E, double *spec, double *ep, double *sp, double *fwhm, long n)
 {
-  long i;
-  
-  *sp=spec[0];
-  *ep=E[0];
-  for (i=1; i<n; i++) {
-    if (*sp<spec[i]) {
-      *sp=spec[i];
-      *ep=E[i];
+  long i, ip;
+  double e1, e2;
+
+  *sp = spec[0];
+  *ep = E[0];
+  ip = -1;
+  for (i = 1; i < n; i++) {
+    if (*sp < spec[i]) {
+      *sp = spec[i];
+      *ep = E[i];
+      ip = i;
     }
+  }
+  if (fwhm && ip != -1) {
+    *fwhm = -1;
+    e1 = e2 = -1;
+    for (i = ip; i >= 1; i--) {
+      if (spec[i] < (spec[ip] / 2)) {
+        e1 = E[i] + (E[i + 1] - E[i]) / (spec[i + 1] - spec[i]) * (spec[ip] / 2 - spec[i]);
+        break;
+      }
+    }
+    for (i = ip; i < (n - 1); i++) {
+      if (spec[i + 1] < (spec[ip] / 2)) {
+        e2 = E[i] + (E[i + 1] - E[i]) / (spec[i + 1] - spec[i]) * (spec[ip] / 2 - spec[i]);
+        break;
+      }
+    }
+    if (e1 > 0 && e2 > 0)
+      *fwhm = e2 - e1;
   }
 }
 
-int Gauss_Convolve(double *E,double *spec,long *ns,double sigmaE, long *n1, long *n2) 
+/* Convolve (E,spec) of length *ns with a Gaussian of relative width sigmaE.
+ * The peak is located internally to scale the kernel.  Output: *ns is
+ * shrunk to the convolved range; convolved arrays are written back into
+ * E and spec.  nstart/nend are optional: if non-NULL, they receive the
+ * begin/end indices into the original input arrays.
+ * Returns 0 on success, 1 on failure. */
+int Gauss_Convolve(double *E, double *spec, long *ns, double sigmaE, long *nstart, long *nend)
 {
-  long nSigma=3,nppSigma=6,ne1,ne2,ns1,np;
-  
-  int i,j;
-  double ep,sp,sigp,de,sum, *gs,x, *spec2;
-	
-  ns1=*ns;
-  gs=spec2=NULL;
-  
+  long nSigma = 3, nppSigma = 6, ne1, ne2, ns1, np;
+  long i, j;
+  double ep, sp, sigp, de, sum, *gs, x, *spec2;
+
+  ns1 = *ns;
+  gs = spec2 = NULL;
+
   if (!E || !spec) {
-    fprintf(stderr,"No energy or spectra points!\n");
-    return 0;
+    fprintf(stderr, "No energy or spectra points!\n");
+    return 1;
   }
-  FindPeak(E,spec,&ep,&sp,ns1);
-  /*generate Gaussian with correct sigma in units of x-axis */
-  de=E[1]-E[0];
-  sigp=2.0*sigmaE*ep/de; /*sigma in x-axis units */
-  
-  if (sigp < (nppSigma-1)) {
-    fprintf(stderr,"too few data points for Gaussian convolution\n");
+  FindPeak(E, spec, &ep, &sp, NULL, ns1);
+  /* generate Gaussian with correct sigma in units of x-axis */
+  de = E[1] - E[0];
+  sigp = 2.0 * sigmaE * ep / de;
+
+  if (sigp < (nppSigma - 1)) {
+    fprintf(stderr, "too few data points for Gaussian convolution\n");
     fprintf(stderr, "de = %e, sigmaE = %e, ep = %e, sigp=%le\n", de, sigmaE, ep, sigp);
-    return 0; 
+    return 1;
   }
-  np=(2*nSigma)*sigp+1;
-  if (np%2==0) np=np+1; /* make odd */
-  gs=(double*)calloc(np,sizeof(*gs));
-  spec2=(double*)calloc(ns1,sizeof(*spec2));
-  sum=0.0;
-  for (i=0;i<np;i++) {
-    x=i*1.0-0.5*(np-1);
-    gs[i]=exp(-x*x/2.0/(sigp*sigp));
-    sum=sum+gs[i];
+  np = (2 * nSigma) * sigp + 1;
+  if (np % 2 == 0)
+    np = np + 1; /* make odd */
+  gs = (double *)calloc(np, sizeof(*gs));
+  spec2 = (double *)calloc(ns1, sizeof(*spec2));
+  sum = 0.0;
+  for (i = 0; i < np; i++) {
+    x = i * 1.0 - 0.5 * (np - 1);
+    gs[i] = exp(-x * x / 2.0 / (sigp * sigp));
+    sum = sum + gs[i];
   }
-  /*make convolution */
-  ne1=np/2;
-  ne2=ns1-ne1-1;
-  if (ne2<0) {
-    fprintf(stderr,"Error: Check the number of peak search points\n");
-    return 0;
+  /* make convolution */
+  ne1 = np / 2;
+  ne2 = ns1 - ne1 - 1;
+  if (ne2 < 0) {
+    fprintf(stderr, "Error: Check the number of peak search points\n");
+    free(spec2);
+    free(gs);
+    return 1;
   }
-  for (i=ne1;i<=ne2;i++) {
-    spec2[i]=0.0;
-    for (j=0;j<np;j++)
-      spec2[i]=spec2[i]+spec[i+ne1-j]*gs[j];
+  for (i = ne1; i <= ne2; i++) {
+    spec2[i] = 0.0;
+    for (j = 0; j < np; j++)
+      spec2[i] = spec2[i] + spec[i + ne1 - j] * gs[j];
   }
-  /*retun in original array and make adjustment of array sizes */
-  *ns=ne2-ne1+1;
-  *n1 = ne1;
-  *n2 = ne2;
-  for (i=ne1;i<=ne2;i++) {
-    E[i-ne1]=E[i];
-    spec[i-ne1]=spec2[i]/sum;
+  /* return in original array and make adjustment of array sizes */
+  *ns = ne2 - ne1 + 1;
+  if (nstart)
+    *nstart = ne1;
+  if (nend)
+    *nend = ne2;
+  for (i = ne1; i <= ne2; i++) {
+    E[i - ne1] = E[i];
+    spec[i - ne1] = spec2[i] / sum;
   }
   free(spec2);
   free(gs);
-  return 1;
+  return 0;
 }
