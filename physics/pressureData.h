54bacd87553d362347246aa3f805fe0a79aab202
rtsoliday <soliday@anl.gov>
2026-07-01 09:05:41 -0500

    Added gas scattering to moments_output command. Added force_mcs to matter command.
    Original author: borland

--- physics/pressureData.h
+++ physics/pressureData.h
@@ -2,9 +2,11 @@
 * Copyright (c) 2017 The University of Chicago, as Operator of Argonne
 * National Laboratory.
 * This file is distributed subject to a Software License Agreement found
-* in the file LICENSE that is included with this distribution. 
+* in the file LICENSE that is included with this distribution.
 \*************************************************************************/
 
+#if !defined(PRESSURE_DATA_H)
+#define PRESSURE_DATA_H 1
 /*
  * Joe Calvey, Michael Borland 2017
  */
@@ -31,13 +33,14 @@ extern "C" {
 #endif
 
 #if (defined(_WIN32) && defined(_MSC_VER))
-__declspec(dllexport) void readGasPressureData(char *filename, PRESSURE_DATA *pressureData);
+__declspec(dllexport) void readGasPressureData(char *filename, PRESSURE_DATA *pressureData, double factor, long verbosity);
 #else
-void readGasPressureData(char *filename, PRESSURE_DATA *pressureData);
+void readGasPressureData(char *filename, PRESSURE_DATA *pressureData, double factor, long verbosity);
 #endif
 void computeAverageGasPressures(double sStart, double sEnd, double *pressure, PRESSURE_DATA *pressureData);
 long identifyGas(GAS_DATA *gasData, char *name);
 
 #ifdef __cplusplus
 }
+#endif
 #endif
